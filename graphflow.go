e238767e81c280bcc321d2ce3e28fbd0e64bb578
Doug Johnson <doug.johnson@futrli.com>
2020-11-10 16:29:34 +0000

    add comments

--- graphflow.go
+++ graphflow.go
@@ -1,3 +1,18 @@
+// Package graphflow supports the building, execution and graphical rendering of simple linear workflows
+//
+// There may be times when complex business logic is best represented in the form of a decision tree or a workflow
+// comprised of some "context" and a series of yes/no questions and actions or decisions that can be made against this
+// context.
+//
+// A naive representation of this in code could be a error-prone block of deeply nested if / else conditions. In such a
+// case it is difficult for a product stakeholder to precisely express what they expect and it's equally difficult for them
+// to validate it once it's built.
+//
+// The graphflow package allows this logic to instead be represented as a self-documenting series of Task nodes with
+// conditional Paths between them. It is self-documenting because it includes the ability to output a graphviz png
+// representation of the entire graph as well as the particular path taken through the graph when provided with a given
+// context.
+//
 package graphflow
 
 import (
@@ -10,15 +25,23 @@ import (
 	"github.com/goccy/go-graphviz/cgraph"
 )
 
+// PathCondition is a type representing the condition that should be satisfied for a certain path
+// to be followed when traversing the graph
 type PathCondition int
 
 const (
+	// ALWAYS is the default PathCondition (as 0 is the default value of an int). A path with the ALWAYS PathCondition
+	// set will always be followed. No other PathCondition can be added to a node if it has an ALWAYS path leaving it.
 	ALWAYS PathCondition = 0
-	YES    PathCondition = 1
-	NO     PathCondition = 2
-	ERROR  PathCondition = 3
+	// YES is the PathCondition that should be set by a question node if its condition passes
+	YES PathCondition = 1
+	// NO is the PathCondition that should be set by a question node if its condition fails
+	NO PathCondition = 2
+	// ERROR is the PathCondition that should be set by any node if you want a particular path to be followed in the event of an error
+	ERROR PathCondition = 3
 )
 
+// PathConditionName is a map back to the textual name for each PathCondition
 var PathConditionName = map[PathCondition]string{
 	0: "ALWAYS",
 	1: "YES",
@@ -26,6 +49,8 @@ var PathConditionName = map[PathCondition]string{
 	3: "ERROR",
 }
 
+// Graphflow represents a series of Tasks with defined Paths between them constructed as a simple workflow.
+// Graphflow methods support its construction, execution and rendering as a graphflow png.
 type Graphflow struct {
 	context  *ExecutionContext
 	tasks    []TaskIntf
@@ -33,14 +58,25 @@ type Graphflow struct {
 	executed map[TaskIntf]bool
 }
 
+// ExecutionContext is a map of values of any type that is passed from Task to Task as the graphflow is executed
+// Tasks are free to read from the context as well as write to it.
+// During the graphflow execution one or more of the Tasks should store the result of the run in the ExecutionContext
+// for retrieval after execution has completed.
+type ExecutionContext struct {
+	values map[string]interface{}
+}
+
+// GetContext retrieves the current ExecutionContext from the graphflow
 func (gf *Graphflow) GetContext() *ExecutionContext {
 	return gf.context
 }
 
+// AddTask adds a new Task (a struct implementing the TaskIntf interface) to the graphflow
 func (gf *Graphflow) AddTask(task TaskIntf) {
 	gf.tasks = append(gf.tasks, task)
 }
 
+// AddPath adds conditional Paths between graphflow Tasks
 func (gf *Graphflow) AddPath(from TaskIntf, condition PathCondition, to TaskIntf) {
 	if gf.paths == nil {
 		gf.paths = make(map[TaskIntf]map[PathCondition]TaskIntf)
@@ -51,8 +87,11 @@ func (gf *Graphflow) AddPath(from TaskIntf, condition PathCondition, to TaskIntf
 	gf.paths[from][condition] = to
 }
 
+// Run passes the graphflow an ExecutionContext and, starting at the StartTask, follows conditional Paths
+// through the graphflow, executing each Task until it reaches the EndTask.
 func (gf *Graphflow) Run(context *ExecutionContext) error {
 	gf.context = context
+	gf.executed = make(map[TaskIntf]bool)
 	err := gf.execute()
 	if err != nil {
 		return err
@@ -61,14 +100,12 @@ func (gf *Graphflow) Run(context *ExecutionContext) error {
 	return nil
 }
 
-type ExecutionContext struct {
-	values map[string]interface{}
-}
-
+// Get retrieves a specific value from the ExecutionContext
 func (ctx *ExecutionContext) Get(v string) interface{} {
 	return ctx.values[v]
 }
 
+// Set sets a specific value in the ExecutionContext, updating it if if already exists
 func (ctx *ExecutionContext) Set(key string, value interface{}) {
 	if ctx.values == nil {
 		ctx.values = make(map[string]interface{})
@@ -76,6 +113,15 @@ func (ctx *ExecutionContext) Set(key string, value interface{}) {
 	ctx.values[key] = value
 }
 
+// TaskIntf is the interface that all graphflow Tasks need to implement.
+// New Tasks should be defined like so:
+//
+//     	type MyNewTask struct {
+//			graphflow.Task
+//		}
+//
+// This ensures that they include the implementation of SetExitPath(PathCondition)
+// provided by graphflow.Task
 type TaskIntf interface {
 	SetExitPath(PathCondition)
 	Execute(*ExecutionContext) error
@@ -83,15 +129,23 @@ type TaskIntf interface {
 	getExitPath() PathCondition
 }
 
+// Task is a struct that all new Tasks should include in their definition like so:
+//
+//     	type MyNewTask struct {
+//			graphflow.Task
+//		}
 type Task struct {
-	Name     string
 	exitPath PathCondition
 }
 
+// Execute is the empty default method on Task that needs to be overridden by
+// Tasks you create
 func (t *Task) Execute(c *ExecutionContext) error {
 	return nil
 }
 
+// SetExitPath needs to be called by Tasks you create in their Execute() method if you want the ExitPath
+// to be anything other than the default PathCondition, ALWAYS
 func (t *Task) SetExitPath(path PathCondition) {
 	t.exitPath = path
 }
@@ -100,24 +154,46 @@ func (t *Task) getExitPath() PathCondition {
 	return t.exitPath
 }
 
+// String is the default implementation of the String() method that needs to be overridden by Tasks you create.
+// It should return a meaningful description of your Task that'll be output in the graphviz png
 func (t *Task) String() string {
 	return "Unnamed TaskIntf"
 }
 
+// StartTask is a Task provided by the package. Every graphflow must include a StartTask.
 type StartTask struct {
 	Task
 }
 
+// String returns the name of the StartTask
 func (t *StartTask) String() string {
 	return "Start"
 }
 
+// EndTask is a Task provided by the package. Every graphflow must include an EndTask.
 type EndTask struct {
 	Task
 }
 
+// String returns the name of the EndTask
 func (t *EndTask) String() string {
 	return "End"
+}
+
+// RenderGraph returns a buffer of bytes containing a graphviz png representation of all the Tasks and the Paths
+// connecting them
+func (gf *Graphflow) RenderGraph() (bytes.Buffer, error) {
+	return gf.generateGraph(false, "")
+}
+
+// RenderPathThroughGraph returns a buffer of bytes containing a graphviz png representation of all the Tasks and the Paths
+// connecting them, with the actual path taken for the given ExecutionContext highlighted. Any Context Keys provided will be
+// rendered with their values at the top of the image.
+func (gf *Graphflow) RenderPathThroughGraph(context *ExecutionContext, contextKeysToRender ...string) (bytes.Buffer, error) {
+	if len(gf.executed) == 0 {
+		gf.Run(context)
+	}
+	return gf.generateGraph(true, contextKeysToRender...)
 }
 
 // BFS (Breadth-First Search) is one of the most widely known algorithms to traverse a graph.
@@ -189,25 +265,14 @@ func (gf *Graphflow) execute() error {
 	return nil
 }
 
-func (wf *Graphflow) findStartTask() (TaskIntf, error) {
-	for _, task := range wf.tasks {
+func (gf *Graphflow) findStartTask() (TaskIntf, error) {
+	for _, task := range gf.tasks {
 		_, isStartTask := task.(*StartTask)
 		if isStartTask {
 			return task, nil
 		}
 	}
 	return nil, errors.New("Workflow needs to contain a task of type StartTask")
-}
-
-func (gf *Graphflow) RenderGraph() (bytes.Buffer, error) {
-	return gf.generateGraph(false, "")
-}
-
-func (gf *Graphflow) RenderPathThroughGraph(context *ExecutionContext, contextKeysToRender ...string) (bytes.Buffer, error) {
-	if len(gf.executed) == 0 {
-		gf.Run(context)
-	}
-	return gf.generateGraph(true, contextKeysToRender...)
 }
 
 func (gf *Graphflow) generateGraph(showPath bool, contextKeysToRender ...string) (bytes.Buffer, error) {
